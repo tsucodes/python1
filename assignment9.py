c1498195192dcf4f8296856076efbe001849fd44
tsucodes <96221592+tsucodes@users.noreply.github.com>
2025-08-18 17:47:42 -0400

    Add files via upload

---
+++ assignment9.py
@@ -0,0 +1,7 @@
+'''
+Challenge: Ensure that the user enters only a single character and handle cases where the input is not a letter.
+=====================================================
+Input: Ask the user to enter a single character.
+Processing: Determine whether the entered character is a vowel (a, e, i, o, u) or a consonant.
+Output: Display whether the entered character is a vowel or a consonant.
+'''
