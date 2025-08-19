a6668952da7449a5bac33728354409db64cc9f4b
tsucodes <96221592+tsucodes@users.noreply.github.com>
2025-08-19 00:22:16 -0400

    Add files via upload

---
+++ assignment13.py
@@ -0,0 +1,14 @@
+#Challenge: Use a loop structure to compare characters from both ends of the string to determine whether it is a palindrome.
+#===================================
+#Description: Write a program that prompts the user to enter a string and then checks whether it is a palindrome.
+# A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward.
+from assignment11 import user_input
+
+#input user enters string
+user1_input = input("check palindrome:")
+#processing
+# break input into individual characters. split string
+#iterate through characters
+# check to see if it is same backwards use operator.?
+# if user1_input is same forward and backward print palindrome
+#challaenge use loop
